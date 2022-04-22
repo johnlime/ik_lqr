1a6f031b43d9e7d933cc80621a50c5cdb8721866
johnlime <thelimejuiceproductions@gmail.com>
2022-04-22 16:35:42 +0900

    calc_end_effector_pos outputs positions for all end effectors

--- run/pigeon_ik.py
+++ run/pigeon_ik.py
@@ -16,21 +16,21 @@ for iter_num in range(1):
 env.close()
 
 
-def calc_end_effector_pos(start_to_end_angles, index = len(start_to_end_angles) - 1):
+def calc_end_effector_pos(body_to_head_angles):
     """
-    start_to_end_angles: array of joint angles starting from those
+    body_to_head_angles: array of joint angles starting from those
     closest to the body to the those furthest from the body
     """
-    assert index < len(start_to_end_angles)
-
-    end_effector = np.array([- BODY_WIDTH, BODY_HEIGHT])
+    end_effector = np.tile(np.array([- BODY_WIDTH, BODY_HEIGHT]), (len(body_to_head_angles), 1)
 
     angle_cumul = 0
-    for i in range(len(start_to_end_angles) - 1):
-        angle_cumul += start_to_end_angles[i]
-        end_effector += 2 * LIMB_WIDTH * np.array([- np.cos(angle_cumul), np.sin(angle_cumul)])
+    i = 0
+    for i in range(len(body_to_head_angles) - 1):
+        angle_cumul += body_to_head_angles[i]
+        end_effector[i:, :] += 2 * LIMB_WIDTH * \
+            np.array([- np.cos(angle_cumul), np.sin(angle_cumul)])
 
-    angle_cumul += start_to_end_angles[i]
-    end_effector += HEAD_WIDTH * np.array([- np.cos(angle_cumul), np.sin(angle_cumul)])
+    angle_cumul += body_to_head_angles[i]
+    end_effector[i:, :] += HEAD_WIDTH * np.array([- np.cos(angle_cumul), np.sin(angle_cumul)])
 
     return end_effector
